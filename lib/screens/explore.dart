775d78e7aaa97823c186e55494e1521b85312d79
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2020-12-04 23:44:32 +0200

    added bottom navigation bar

---
+++ lib/screens/explore.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Explore extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Explore'),
+    );
+  }
+}

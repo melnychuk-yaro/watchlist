b0aa9347348aac08a8f0b3bbebbca920b3707949
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2020-12-04 22:20:40 +0200

    set firebase initiation and auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:watchlist/screens/favorites.dart';
-import 'package:watchlist/screens/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:watchlist/screens/favorites-screen.dart';
+import 'package:watchlist/screens/login-screen.dart';
+import 'package:watchlist/themes.dart';
 
 void main() {
   runApp(WatchListApp());
@@ -12,13 +15,42 @@ class WatchListApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Watchlist',
-      theme: ThemeData.dark().copyWith(
-        accentColor: Colors.indigoAccent,
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.indigoAccent,
-        ),
-      ),
-      home: LoginScreen(),
+      theme: DefautTheme().theme,
+      home: FutureBuilder<Object>(
+          future: Firebase.initializeApp(),
+          builder: (context, snapshot) {
+            // Check for errors
+            if (snapshot.hasError) {
+              return ErrorText();
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return StreamBuilder<Object>(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return ErrorText();
+                  }
+
+                  if (snapshot.data == null) {
+                    return LoginScreen();
+                  } else {
+                    return FavoritesScreen();
+                  }
+                },
+              );
+            }
+            return Center(child: CircularProgressIndicator());
+          }),
+    );
+  }
+}
+
+class ErrorText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          'Something went wrong. Please try again later or contact support.'),
     );
   }
 }

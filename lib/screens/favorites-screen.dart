0d0acbe026527e99ae0481fb5580929ebdeb0b2e
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2020-12-04 22:19:19 +0200

    rename favorites screen

--- lib/screens/favorites-screen.dart
+++ lib/screens/favorites-screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Favorites extends StatelessWidget {
+class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(

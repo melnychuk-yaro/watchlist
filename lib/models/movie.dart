e5a2aa864f77d3b0723185a05068a56de9788cf6
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2020-12-13 00:25:54 +0200

    refactor model, added fromMap method

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,19 +1,33 @@
 class Movie {
-  final String id;
+  final int id;
   final String title;
-  final String year;
-  final String release;
-  final String director;
-  final String plot;
+  final String releaseDate;
   final String poster;
+  final double rating;
+  static const imgPath = 'https://image.tmdb.org/t/p/w500';
 
-  Movie(
+  Movie({
     this.id,
     this.title,
-    this.year,
-    this.release,
-    this.director,
-    this.plot,
+    this.releaseDate,
     this.poster,
-  );
+    this.rating,
+  });
+
+  factory Movie.fromMap(Map<String, dynamic> movieMap) {
+    Movie movie = Movie(
+      id: movieMap['id'],
+      title: movieMap['title'],
+      releaseDate: movieMap['release_date'] ?? '',
+      poster:
+          movieMap['poster_path'] == null || movieMap['poster_path'] == 'null'
+              ? 'https://critics.io/img/movies/poster-placeholder.png'
+              : imgPath + movieMap['poster_path'],
+      rating:
+          movieMap['vote_average'] == null || movieMap['vote_average'] == 'null'
+              ? 0.0
+              : movieMap['vote_average'].toDouble(),
+    );
+    return movie;
+  }
 }

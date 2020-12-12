804ad4f5dc1baa01838d9dafde3a02dff8c88d45
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2020-12-13 00:27:34 +0200

    added movies repository

---
+++ lib/services/movies_repository.dart
@@ -0,0 +1,10 @@
+import 'package:watchlist/models/movie.dart';
+import 'package:watchlist/services/movies_provider.dart';
+
+class MoviesRepository {
+  MoviesProvider _moviesProvider = MoviesProvider();
+  Future<List<Movie>> getTopMovies() => _moviesProvider.getTopRatedMovies();
+  Future<List<Movie>> getNewMovies() => _moviesProvider.getNewMovies();
+  Future<List<Movie>> searchMovies(query) =>
+      _moviesProvider.searchMovies(query);
+}

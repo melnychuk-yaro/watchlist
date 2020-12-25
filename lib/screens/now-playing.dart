7f79a2dcf5dc600158da8c21af5eaba95678cac2
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2020-12-25 17:03:46 +0200

    simplified naming

--- lib/screens/now-playing.dart
+++ lib/screens/now-playing.dart
@@ -18,47 +18,45 @@ class _NowPlayingState extends State<NowPlaying> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: BlocBuilder<MoviesBloc, MoviesState>(
-        builder: (context, state) {
-          if (state is MoviesInitialState) {
-            return Center(
-              child: Text(
-                'No data. Please try again later.',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            );
-          }
+    return BlocBuilder<MoviesBloc, MoviesState>(
+      builder: (context, state) {
+        if (state is MoviesInitial) {
+          return Center(
+            child: Text(
+              'No data. Please try again later.',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          );
+        }
 
-          if (state is MoviesErrorState) {
-            return Center(
-              child: Text(
-                'Something went wrong. Please try again later.',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Theme.of(context).errorColor,
-                ),
+        if (state is MoviesError) {
+          return Center(
+            child: Text(
+              'Something went wrong. Please try again later.',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Theme.of(context).errorColor,
               ),
-            );
-          }
+            ),
+          );
+        }
 
-          if (state is MoviesLoadedState) {
-            return GridView.builder(
-              itemCount: state.loadedMovies.length,
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 300.0,
-                childAspectRatio: 0.75,
-              ),
-              itemBuilder: (context, index) {
-                return MovieCard(movie: state.loadedMovies[index]);
-              },
-            );
-          }
+        if (state is MoviesLoaded) {
+          return GridView.builder(
+            padding: const EdgeInsets.all(4.0),
+            itemCount: state.loadedMovies.length,
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 300.0,
+              childAspectRatio: 0.75,
+            ),
+            itemBuilder: (context, index) {
+              return MovieCard(movie: state.loadedMovies[index]);
+            },
+          );
+        }
 
-          return Center(child: CircularProgressIndicator());
-        },
-      ),
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }

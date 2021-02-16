a9a579a28302711962997783085ddae049d5f9e9
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-02-16 22:06:14 +0200

    removed fromSnapshot refactor poster handling

--- lib/data/models/movie.dart
+++ lib/data/models/movie.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 @immutable
@@ -6,47 +5,36 @@ class Movie {
   final int id;
   final String title;
   final String releaseDate;
-  final String poster;
   final double rating;
-  static const imgPath = 'https://image.tmdb.org/t/p/w500';
+  final bool isFavorite = false;
+  final String posterFileName;
+  static const posterPath = 'https://image.tmdb.org/t/p/w500';
 
   Movie({
     @required this.id,
     @required this.title,
     @required this.releaseDate,
-    @required this.poster,
+    @required this.posterFileName,
     @required this.rating,
+    bool isFavorite,
   });
+
+  String get fullPosterPath => posterPath + posterFileName;
 
   factory Movie.fromMap(Map<String, dynamic> movieMap) {
     return Movie(
       id: movieMap['id'],
       title: movieMap['title'],
       releaseDate: movieMap['release_date'] ?? '',
-      poster:
-          movieMap['poster_path'] == null || movieMap['poster_path'] == 'null'
-              ? ''
-              : imgPath + movieMap['poster_path'],
+      posterFileName: movieMap['poster_path'] == null ||
+              movieMap['poster_path'] == 'null' ||
+              movieMap['poster_path'] == ''
+          ? ''
+          : movieMap['poster_path'],
       rating:
           movieMap['vote_average'] == null || movieMap['vote_average'] == 'null'
               ? 0.0
               : movieMap['vote_average'].toDouble(),
-    );
-  }
-
-  factory Movie.fromSnapshot(QueryDocumentSnapshot movieSnapshot) {
-    return Movie(
-      id: movieSnapshot['id'],
-      title: movieSnapshot['title'],
-      releaseDate: movieSnapshot['release_date'] ?? '',
-      poster:
-          movieSnapshot['poster'] == null || movieSnapshot['poster'] == 'null'
-              ? Placeholder()
-              : movieSnapshot['poster'],
-      rating:
-          movieSnapshot['rating'] == null || movieSnapshot['rating'] == 'null'
-              ? 0.0
-              : movieSnapshot['rating'].toDouble(),
     );
   }
 
@@ -55,8 +43,8 @@ class Movie {
       'id': this.id,
       'title': this.title,
       'release_date': this.releaseDate,
-      'poster': this.poster,
-      'rating': this.rating
+      'poster_path': this.posterFileName,
+      'vote_average': this.rating
     };
   }
 }

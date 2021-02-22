52b95d7cfaee84d2e993fc75e478d15e3c21dca1
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-02-22 17:55:19 +0200

    const constructors are better

--- lib/business_logic/bloc/favorites_state.dart
+++ lib/business_logic/bloc/favorites_state.dart
@@ -3,34 +3,35 @@ part of 'favorites_bloc.dart';
 @immutable
 abstract class FavoritesState {
   final List<Movie> loadedMovies;
-  FavoritesState(this.loadedMovies);
+  const FavoritesState(this.loadedMovies);
 }
 
 class FavoritesInitial extends FavoritesState {
-  FavoritesInitial(loadedMovies) : super(loadedMovies);
+  const FavoritesInitial(loadedMovies) : super(loadedMovies);
 }
 
 class FavoritesLoading extends FavoritesState {
-  FavoritesLoading(loadedMovies) : super(loadedMovies);
+  const FavoritesLoading(loadedMovies) : super(loadedMovies);
 }
 
 class FavoritesLoaded extends FavoritesState {
   final List<Movie> loadedMovies;
-  FavoritesLoaded({@required this.loadedMovies}) : super(loadedMovies);
+  const FavoritesLoaded({@required this.loadedMovies}) : super(loadedMovies);
 }
 
 class FavoritesAdding extends FavoritesState {
-  FavoritesAdding(loadedMovies) : super(loadedMovies);
+  const FavoritesAdding(loadedMovies) : super(loadedMovies);
 }
 
 class FavoritesAdded extends FavoritesState {
   final List<Movie> loadedMovies;
   final Movie newFavMovie;
-  FavoritesAdded({@required this.loadedMovies, @required this.newFavMovie})
+  const FavoritesAdded(
+      {@required this.loadedMovies, @required this.newFavMovie})
       : super(loadedMovies);
 }
 
 class FavoritesError extends FavoritesState {
   final List<Movie> loadedMovies;
-  FavoritesError(this.loadedMovies) : super(loadedMovies);
+  const FavoritesError(this.loadedMovies) : super(loadedMovies);
 }

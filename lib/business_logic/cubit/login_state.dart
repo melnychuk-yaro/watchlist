d0ebcaca3dcc9db96e8b715ec722ad494a1e9780
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-03-24 14:32:02 +0200

    add error message

--- lib/business_logic/cubit/login_state.dart
+++ lib/business_logic/cubit/login_state.dart
@@ -5,24 +5,28 @@ class LoginState extends Equatable {
     this.email = const Email.pure(),
     this.password = const Password.pure(),
     this.status = FormzStatus.pure,
+    this.errorMsg = '',
   });
 
   final Email email;
   final Password password;
   final FormzStatus status;
+  final String errorMsg;
 
   @override
-  List<Object> get props => [email, password, status];
+  List<Object> get props => [email, password, status, errorMsg];
 
   LoginState copyWith({
     Email? email,
     Password? password,
     FormzStatus? status,
+    String? errorMsg,
   }) {
     return LoginState(
       email: email ?? this.email,
       password: password ?? this.password,
       status: status ?? this.status,
+      errorMsg: errorMsg ?? this.errorMsg,
     );
   }
 }

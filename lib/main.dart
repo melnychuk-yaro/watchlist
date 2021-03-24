686c6827fd6218fbcedcdad41e086b2be6739171
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-03-24 14:27:23 +0200

    add signup cubit

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ import 'package:watchlist/business_logic/bloc/search_bloc.dart';
 import 'package:watchlist/business_logic/bloc/auth_bloc.dart';
 import 'package:watchlist/business_logic/cubit/all_favorites_cubit.dart';
 import 'package:watchlist/business_logic/cubit/now_playing_cubit.dart';
+import 'package:watchlist/business_logic/cubit/sign_up_cubit.dart';
 import 'package:watchlist/business_logic/cubit/top_movies_cubit.dart';
 import 'package:watchlist/data/repositories/auth_repository.dart';
 import 'package:watchlist/data/repositories/movies_repository.dart';
 import 'package:watchlist/presentation/screens/home-screen.dart';
-import 'package:watchlist/presentation/screens/login-screen.dart';
+import 'package:watchlist/presentation/screens/auth-screen.dart';
 import 'package:watchlist/presentation/themes/app_theme.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
@@ -33,6 +34,7 @@ class App extends StatelessWidget {
       providers: [
         BlocProvider(create: (_) => AuthBloc(_authRepository)),
         BlocProvider(create: (_) => LoginCubit(_authRepository)),
+        BlocProvider(create: (_) => SignUpCubit(_authRepository)),
       ],
       child: MaterialApp(
         title: 'Watchlist',
@@ -70,7 +72,7 @@ class App extends StatelessWidget {
                     ],
                     child: HomeScreen(),
                   )
-                : LoginScreen();
+                : AuthScreen();
           },
         ),
       ),

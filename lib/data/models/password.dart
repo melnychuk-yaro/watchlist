9777a91714ef8edd790f2526b37e3d846e6c574f
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-03-24 14:28:20 +0200

    change password validator

--- lib/data/models/password.dart
+++ lib/data/models/password.dart
@@ -1,18 +1,15 @@
 import 'package:formz/formz.dart';
 
-enum PasswordValidationError { invalid }
+enum PasswordValidationError { short }
 
 class Password extends FormzInput<String, PasswordValidationError> {
   const Password.pure() : super.pure('');
   const Password.dirty([String value = '']) : super.dirty(value);
 
-  static final _passwordRegExp =
-      RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
-
   @override
   PasswordValidationError? validator(String? value) {
-    return _passwordRegExp.hasMatch(value ?? '')
-        ? null
-        : PasswordValidationError.invalid;
+    if (pure) return null;
+    value = value ?? '';
+    return value.length < 6 ? PasswordValidationError.short : null;
   }
 }

7116543e2c21e4031180a9397a7f64d0448c0840
Yaro Melnychuk <melnychuk.yaro@gmail.com>
2021-04-01 18:26:35 +0300

    add skeleton animation

--- lib/presentation/screens/movie_screen/movie_screen.dart
+++ lib/presentation/screens/movie_screen/movie_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 
 import '../../../business_logic/cubit/single_movie_cubit.dart';
 import '../../../constatns.dart';
-import '../../../data/models/movie_detailed.dart';
 import '../../widgets/styled_text.dart';
 import 'backdrop.dart';
+import 'custom_back_button.dart';
 import 'genres.dart';
 import 'info_item.dart';
 import 'poster.dart';
@@ -14,14 +15,23 @@ import 'toolbar.dart';
 import 'youtube_player.dart';
 
 class MovieScreen extends StatefulWidget {
-  const MovieScreen({required this.id});
+  const MovieScreen({required this.id, required this.title});
   final int id;
+  final String title;
 
   @override
   _MovieScreenState createState() => _MovieScreenState();
 }
 
 class _MovieScreenState extends State<MovieScreen> {
+  static const double posterWidth = 120.0;
+  static const double backdropHeight = 250.0;
+  final budgetFormat = NumberFormat.currency(
+    locale: 'en_US',
+    symbol: '\$',
+    decimalDigits: 0,
+  );
+
   @override
   void initState() {
     super.initState();
@@ -34,17 +44,180 @@ class _MovieScreenState extends State<MovieScreen> {
     return Scaffold(
       body: BlocBuilder<SingleMovieCubit, SingleMovieState>(
         builder: (context, state) {
-          if (state is SingleMovieLoaded) {
-            return _SingleMovie(movie: state.movie);
-          }
           if (state is SingleMovieError) {
             return StyledText(
               text: state.error,
               icon: Icons.error,
             );
           }
-          return Center(
-            child: CircularProgressIndicator(),
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    state is SingleMovieLoaded
+                        ? Backdrop(
+                            backdropPath: state.movie.fullBackgroundPath,
+                            backdropHeight: backdropHeight,
+                          )
+                        : SkeletonAnimation(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(kBorderRadius * 2),
+                            ),
+                            child: Container(
+                              height: backdropHeight,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).cardColor,
+                                boxShadow: kShadow,
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft:
+                                      Radius.circular(kBorderRadius * 2),
+                                ),
+                              ),
+                            ),
+                          ),
+                    Positioned(
+                      top: 143,
+                      left: 16,
+                      child: state is SingleMovieLoaded
+                          ? Poster(
+                              posterPath: state.movie.fullPosterPath,
+                              posterWidth: posterWidth,
+                            )
+                          : _PosterSkeleton(posterWidth: posterWidth),
+                    ),
+                    Positioned(
+                      top: 215,
+                      right: 0,
+                      child: state is SingleMovieLoaded
+                          ? ToolBar(movie: state.movie)
+                          : _ToolBarSkeleton(),
+                    ),
+                    CustomBackButton(),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(kPadding),
+                  child: Row(
+                    children: [
+                      Container(width: posterWidth),
+                      const SizedBox(width: kPadding),
+                      Expanded(
+                        child: state is SingleMovieLoaded
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  InfoItem(
+                                    defenition: 'Rating:',
+                                    value: state.movie.rating.toString(),
+                                  ),
+                                  const SizedBox(height: 4.0),
+                                  Flexible(
+                                    child: InfoItem(
+                                      defenition: 'Release Date:',
+                                      value: state.movie.releaseDate,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4.0),
+                                  state.movie.budget == 0
+                                      ? InfoItem(
+                                          defenition: 'Budget:', value: '-')
+                                      : InfoItem(
+                                          defenition: 'Budget:',
+                                          value:
+                                              '${budgetFormat.format(state.movie.budget)}',
+                                        ),
+                                ],
+                              )
+                            : Wrap(
+                                direction: Axis.vertical,
+                                spacing: 4.0,
+                                children: [
+                                  const _TextSceleton(width: 120),
+                                  const _TextSceleton(width: 120),
+                                  const _TextSceleton(width: 120),
+                                ],
+                              ),
+                      )
+                    ],
+                  ),
+                ),
+                state is SingleMovieLoaded
+                    ? Genres(genres: state.movie.genres)
+                    : const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: kPadding),
+                        child: _TextSceleton(width: 120, height: 32),
+                      ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(kPadding),
+                    child: state is SingleMovieLoaded
+                        ? Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  widget.title,
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ),
+                              if (state.movie.tagline != '')
+                                const SizedBox(height: kPadding / 2),
+                              if (state.movie.tagline != '')
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: kPadding / 2),
+                                    child: Text(
+                                      state.movie.tagline,
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                    ),
+                                  ),
+                                ),
+                              Flexible(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(top: kPadding),
+                                  child: Text(
+                                    state.movie.overview,
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ),
+                              if (state.movie.youtubeVideoId != '')
+                                const SizedBox(height: kPadding),
+                              if (state.movie.youtubeVideoId != '')
+                                YouTubePlayer(
+                                    youtubeVideoId: state.movie.youtubeVideoId)
+                            ],
+                          )
+                        : Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  widget.title,
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ),
+                              const SizedBox(height: kPadding / 2),
+                              const _TextSceleton(height: 14, width: 120),
+                              const SizedBox(height: kPadding / 2),
+                              const _TextSceleton(height: 120),
+                              const SizedBox(height: kPadding),
+                              const _YouTubeSkeleton(),
+                            ],
+                          ),
+                  ),
+                )
+              ],
+            ),
           );
         },
       ),
@@ -52,120 +225,105 @@ class _MovieScreenState extends State<MovieScreen> {
   }
 }
 
-class _SingleMovie extends StatelessWidget {
-  _SingleMovie({Key? key, required this.movie}) : super(key: key);
-  final MovieDetailed movie;
-  static const double posterWidth = 120;
-  final budgetFormat = NumberFormat.currency(
-    locale: 'en_US',
-    symbol: '\$',
-    decimalDigits: 0,
-  );
+class _YouTubeSkeleton extends StatelessWidget {
+  const _YouTubeSkeleton({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Backdrop(backdropPath: movie.fullBackgroundPath),
-              Positioned(
-                top: 143,
-                left: 16,
-                child: Poster(
-                  posterPath: movie.fullPosterPath,
-                  posterWidth: posterWidth,
-                ),
-              ),
-              Positioned(
-                top: 215,
-                right: 0,
-                child: ToolBar(movie: movie),
-              ),
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: kPadding, top: kPadding),
-                  child: Container(
-                    width: 48,
-                    height: 48,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                      borderRadius: BorderRadius.circular(kBorderRadius / 2),
-                      boxShadow: kShadow,
-                    ),
-                    child: MediaQuery.of(context).platformBrightness ==
-                            Brightness.dark
-                        ? BackButton(color: kLightText)
-                        : BackButton(color: kDarkText),
-                  ),
-                ),
-              ),
-            ],
+    return SkeletonAnimation(
+      borderRadius: BorderRadius.circular(kBorderRadius),
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            boxShadow: kShadow,
+            borderRadius: BorderRadius.circular(kBorderRadius),
           ),
-          Padding(
-            padding: const EdgeInsets.all(kPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    Container(width: posterWidth),
-                    Padding(
-                      padding: const EdgeInsets.only(left: kPadding),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          InfoItem(
-                            defenition: 'Rating:',
-                            value: movie.rating.toString(),
-                          ),
-                          const SizedBox(height: 4),
-                          InfoItem(
-                            defenition: 'Release Date:',
-                            value: movie.releaseDate,
-                          ),
-                          const SizedBox(height: 4),
-                          movie.budget == 0
-                              ? InfoItem(defenition: 'Budget:', value: '-')
-                              : InfoItem(
-                                  defenition: 'Budget:',
-                                  value: '${budgetFormat.format(movie.budget)}',
-                                ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                if (movie.genres.isNotEmpty) const SizedBox(height: 20),
-                if (movie.genres.isNotEmpty) Genres(genres: movie.genres),
-                const SizedBox(height: kPadding),
-                Text(
-                  movie.title,
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                if (movie.tagline != '') const SizedBox(height: kPadding / 2),
-                if (movie.tagline != '')
-                  Text(
-                    movie.tagline,
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                const SizedBox(height: kPadding / 2),
-                Text(
-                  movie.overview,
-                  style: TextStyle(fontSize: kPadding),
-                ),
-                if (movie.youtubeVideoId != '')
-                  const SizedBox(height: kPadding),
-                if (movie.youtubeVideoId != '')
-                  YouTubePlayer(youtubeVideoId: movie.youtubeVideoId)
-              ],
-            ),
-          )
-        ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TextSceleton extends StatelessWidget {
+  const _TextSceleton({
+    Key? key,
+    this.height = 16.0,
+    this.width = double.infinity,
+  }) : super(key: key);
+
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SkeletonAnimation(
+      borderRadius: BorderRadius.circular(kBorderRadius),
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(kBorderRadius),
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterSkeleton extends StatelessWidget {
+  const _PosterSkeleton({
+    Key? key,
+    required this.posterWidth,
+  }) : super(key: key);
+
+  final double posterWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return SkeletonAnimation(
+      borderRadius: BorderRadius.circular(kBorderRadius),
+      child: Container(
+        width: posterWidth,
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(kBorderRadius),
+          boxShadow: kShadow,
+        ),
+        child: AspectRatio(
+          aspectRatio: 2 / 3,
+        ),
+      ),
+    );
+  }
+}
+
+class _ToolBarSkeleton extends StatelessWidget {
+  const _ToolBarSkeleton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SkeletonAnimation(
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(kBorderRadius),
+        topLeft: Radius.circular(kBorderRadius),
+      ),
+      child: Container(
+        height: 64.0,
+        width: 64.0,
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          boxShadow: kShadow,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(kBorderRadius),
+            topLeft: Radius.circular(kBorderRadius),
+          ),
+        ),
       ),
     );
   }
